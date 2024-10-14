942425707a4e755e1465131044bb7d1a724b3818
F00LIM4N <leoaznargarrido@gmail.com>
2024-10-14 14:36:32 +0200

    :construction: début de la page d'accueil

--- assets/app.js
+++ assets/app.js
@@ -6,4 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
